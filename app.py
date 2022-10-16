5465c99a1e3e4bc72c6d7aa83c806493d77f8ee8
DenMat <denisburg_new@mail.ru>
2022-10-16 21:33:38 +0300

    second

--- app.py
+++ app.py
@@ -3,10 +3,25 @@
 from flask import Flask, json, request
 import logging
 
+import socket
+
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
+def extract_ip():
+    st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        st.connect(('10.255.255.255', 1))
+        IP = st.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        st.close()
+    return IP
+
 @app.route('/')
 def index():
-    return "<p>Привет Матвейка! <br>Это наше первое приложение на Питоне в сети Интернет!<br>Ура!</p>"
+    myIP = extraxt_ip()
+    str = f'<p>Привет Матвейка! <br>Это наше первое приложение на Питоне в сети Интернет!<br>Ура!<br>{myIP}</p>'
+    return str
