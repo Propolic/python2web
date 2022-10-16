ed3184292faeaf3326a593279fa4eb2ef9607d0c
DenMat <denisburg_new@mail.ru>
2022-10-16 21:23:38 +0300

    ferst

---
+++ app.py
@@ -0,0 +1,12 @@
+# print("Привет мир!")
+
+from flask import Flask, json, request
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<p>Привет Матвейка! <br>Это наше первое приложение на Питоне в сети Интернет!<br>Ура!</p>"
